6592ed361274573a5630f0d6d168c1debbda3f52
Cygnatius <cygnatius@hotmail.com>
2021-08-12 22:55:11 +1000

    Introduced multiple coins

--- app.js
+++ app.js
@@ -5,36 +5,82 @@ const app = Vue.createApp({
             // flips: ['heads', 'tails', 'heads', 'heads', 'tails'],
             flips: [],
             headsimg: 'img/headslg.png',
-            tailsimg: 'img/tailslg.png'
+            tailsimg: 'img/tailslg.png',
+            coins: 1
         }
     },
     methods: {
         coinflip() {
-            if (Math.random()*2 > 1) {
-                this.flips.unshift('heads')
-            } else {
-                this.flips.unshift('tails')
+            let set = [];
+            for (i = 0; i < this.coins; i++) {
+                if (Math.random()*2 > 1) {
+                    set.unshift('heads');
+                } else {
+                    set.unshift('tails');
+                }
             }
+            this.flips.unshift(set);
         }
     },
     computed: {
+        lastFlipCoinCount() {
+            if(this.flips.length == 0) {
+                return 0;
+            } else {
+                let last = this.flips[0];
+                return last.length;
+            }
+        },
+        lastFlipCoinCount2() {
+            return 1;
+        },
         headsFlipped() {
-            heads = 0
-            this.flips.forEach(function(flip) {
-                if(flip == 'heads') {
-                    heads = heads +1
-                }
-            })
-            return heads
+            let heads = 0;
+            this.flips.forEach(function(set) {
+                set.forEach(function(flip) {
+                    if(flip == 'heads') {
+                        heads = heads + 1;
+                    }
+                });
+            });
+            return heads;
         },
         tailsFlipped() {
-            tails = 0
-            this.flips.forEach(function(flip) {
-                if(flip == 'tails') {
-                    tails = tails +1
-                }
-            })
-            return tails
+            let tails = 0;
+            this.flips.forEach(function(set) {
+                set.forEach(function(flip) {
+                    if(flip == 'tails') {
+                        tails = tails + 1;
+                    }
+                });
+            });
+            return tails;
+        },
+        headsFlippedThisSet() {
+            let heads = 0;
+            if(this.flips.length == 0) {
+                heads = 0;
+            } else {
+                this.flips[0].forEach(function(flip) {
+                    if(flip == 'heads') {
+                        heads = heads + 1;
+                    }
+                });
+            }
+            return heads;
+        },
+        tailsFlippedThisSet() {
+            let tails = 0;
+            if(this.flips.length == 0) {
+                tails = 0;
+            } else {
+                this.flips[0].forEach(function(flip) {
+                    if(flip == 'tails') {
+                        tails = tails + 1;
+                    }
+                });
+            }
+            return tails;
         }
     }
-})
+}).mount('#app')
